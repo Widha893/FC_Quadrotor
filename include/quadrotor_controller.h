855d8a96bd3f8703470a644dcc951ede70df2ca5
Widha893 <widha.rizqika.prasetya@mail.ugm.ac.id>
2024-12-10 17:28:32 +0700

    checksum mismatch

--- include/quadrotor_controller.h
+++ include/quadrotor_controller.h
@@ -9,6 +9,7 @@ double torque_x, torque_y, torque_z, total_force;
 double roll_integrator, pitch_integrator, yaw_integrator, altitude_integrator;
 double kp_roll, kd_roll, kp_pitch, kd_pitch, kp_yaw, kd_yaw, kp_altitude, kd_altitude;
 double roll, roll_vel, pitch, pitch_vel, yaw, yaw_vel, altitude;
+double roll_setpoint, pitch_setpoint;
 double alt_last, alt_now, alt_vel;
 double ki_roll = 0.0;
 double ki_pitch = 0.0;
@@ -24,16 +25,16 @@ void init_gains() {
     kd_altitude = 0.0;
 }
 
-void hitl_controller(double setpoint, double vz, double target_alt, uint8_t dt) {
+void hitl_controller(double setpoint, double setpoint_roll, double setpoint_pitch, double vz, double target_alt, uint8_t dt) {
     alt_last = alt_now;
     alt_now = altitude;
     // alt_vel = (alt_now - alt_last) / dt;
-    double error_roll = setpoint - roll;
-    double error_roll_dt = error_roll / dt;
-    double error_pitch = setpoint - pitch;
-    double error_pitch_dt = error_pitch / dt;
+    double error_roll = setpoint_roll - roll;
+    double error_roll_dt = setpoint_roll - roll_vel;
+    double error_pitch = setpoint_roll - pitch;
+    double error_pitch_dt = setpoint_roll - pitch_vel;
     double error_yaw = setpoint - yaw;
-    double error_yaw_dt = error_yaw / dt;
+    double error_yaw_dt = setpoint - yaw_vel;
     double error_altitude = target_alt - altitude;
 
     if (abs(error_roll) < 10.0f) {
@@ -45,10 +46,10 @@ void hitl_controller(double setpoint, double vz, double target_alt, uint8_t dt) 
         pitch_integrator = constrain(pitch_integrator, -INTEGRAL_LIMIT, INTEGRAL_LIMIT);
     }
 
-    total_force = (-kp_altitude * error_altitude) + (-kd_altitude * alt_vel);
-    torque_x = (-kp_roll * error_roll) + (-kd_roll * roll_vel) + (-roll_integrator*ki_roll);
-    torque_y = (-kp_pitch * error_pitch) + (-kd_pitch * pitch_vel) + (-pitch_integrator*ki_pitch);
-    torque_z = (-kd_yaw * yaw_vel);
+    total_force = (kp_altitude * error_altitude) + (kd_altitude * alt_vel);
+    torque_x = (kp_roll * error_roll) + (kd_roll * error_roll_dt) + (roll_integrator*ki_roll);
+    torque_y = (kp_pitch * error_pitch) + (kd_pitch * error_pitch_dt) + (pitch_integrator*ki_pitch);
+    torque_z = (kd_yaw * error_yaw_dt);
 }
 
 #endif
--- src/hitl.cpp
+++ src/hitl.cpp
@@ -6,6 +6,11 @@
 #define LOOP_INTERVAL 10000 // make sure the loop runs exactly every 10 ms
 
 uint32_t loop_timer;
+unsigned long previous_millis = 0;  // Stores the last time the setpoint was changed
+unsigned long interval_on = 2000;    // 2 seconds to stay at 25 degrees
+unsigned long interval_off = 5000;   // 5 seconds cycle
+bool is_on = false;  // Flag to check if we are in the "on" state
+double disturbance;
 
 void setup() {
     Serial.begin(9600);
@@ -15,6 +20,27 @@ void setup() {
 }
 
 void loop() {  
+    uint32_t current_time = micros();  // Get current time in microseconds
+    float dt = (current_time - loop_timer);
+    unsigned long current_millis = millis();
+
+    if (current_millis - previous_millis >= (is_on ? interval_on : interval_off)) {
+        // Save the last time a change was made
+        previous_millis = current_millis;
+
+        // Toggle the is_on flag
+        is_on = !is_on;
+    }
+
+    // Set roll and pitch setpoints based on the is_on flag
+    if (is_on) {
+        disturbance = 0.3f;  // Set to 25 degrees
+        // pitch_setpoint = 25.0f; // Set to 25 degrees
+    } else {
+        disturbance = 0.0f;   // Set to 0 degrees
+        // pitch_setpoint = 0.0f;  // Set to 0 degrees
+    }
+
     if (receive_message()) {
         if (msg.has_gains) {
             kp_roll = msg.gains.roll;
@@ -42,13 +68,49 @@ void loop() {
             yaw_vel = msg.sensors.angular_vel_z * RAD_TO_DEG;
             altitude = msg.sensors.altitude * 100.0;
         }
-        alt_vel = (alt_now - alt_last) / DT;
-        hitl_controller(0.0, alt_vel, 150.0, DT);
+        alt_vel = (alt_now - alt_last) / dt;
+        hitl_controller(0.0, 0.0, 0.0, alt_vel, 150.0, dt);
+
+        // Serial7.print(" ");
+        // Serial7.println(pitch_setpoint);
+
         msg.controls.torque_x = torque_x;
         msg.controls.torque_y = torque_y;
         msg.controls.torque_z = torque_z;
         msg.controls.total_force = total_force;
         msg.has_controls = true;
+        // Serial7.print("kp_roll");
+        // Serial7.print(kp_roll);
+        // Serial7.print(" ");
+        // Serial7.print("kd_roll");
+        // Serial7.print(kd_roll);
+        // Serial7.print(" ");
+        // Serial7.print("kp_pitch");
+        // Serial7.print(kp_pitch);
+        // Serial7.print(" ");
+        // Serial7.print("kd_pitch");
+        // Serial7.print(kd_pitch);
+        // Serial7.print(" ");
+        // Serial7.print("kp_yaw");
+        // Serial7.print(kp_yaw);
+        // Serial7.print(" ");
+        // Serial7.print("kd_yaw");
+        // Serial7.print(kd_yaw);
+        // Serial7.print(" ");
+        // Serial7.print("kp_altitude");
+        // Serial7.print(kp_altitude);
+        // Serial7.print(" ");
+        // Serial7.print("kd_altitude");
+        // Serial7.print(kd_altitude);
+        // Serial7.print(" ");
+        // Serial7.print("roll");
+        // Serial7.print(roll);
+        // Serial7.print(" ");
+        // Serial7.print("roll_vel");
+        // Serial7.print(roll_vel);
+        // Serial7.print(" ");
+        // Serial7.print("pitch");
+        // Serial7.println(pitch);
         send_message(msg);
         // Serial7.println(torque_x);
         // Serial7.println(torque_y);
@@ -58,6 +120,5 @@ void loop() {
         // Serial7.println(alt_vel);
         // Serial7.println(total_force);
     }
-    while (micros() - loop_timer < LOOP_INTERVAL) {}
     loop_timer = micros();
 }
